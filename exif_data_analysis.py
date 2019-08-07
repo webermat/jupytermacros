58baf3bd46364c32851a1303f1bce44d441df0cb
Matthias Artur Weber <matthias.artur.weber@cern.ch>
2019-08-07 20:30:22 +0200

    first dummy analyzer file

---
+++ exif_data_analysis.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+
+def run_analysis():
+
+    file_list=[]
+
+    for input in range(2006,2019):
+        file_list.append(pd.read_csv("/Users/matthiasweber/outputEXIFcsvs/PhotoEXIFDataDayType_%s.csv"%(str(input))))
+    
+
+        print (file_list[0])
+
+
+run_analysis()
