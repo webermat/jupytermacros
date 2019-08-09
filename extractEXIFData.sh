e9bbf49d6675697daa0d0844b46d495e04e151e8
Matthias Artur Weber <matthias.artur.weber@cern.ch>
2019-08-09 17:22:26 +0200

    extract further feature like Aperture,LightValues,ColorInfo

--- extractEXIFData.sh
+++ extractEXIFData.sh
@@ -3,5 +3,5 @@
 for i in {2006..2019}
 do
     echo "extract info from folder $i"
-    exiftool -DateTimeOriginal -d "%b %d %a ,%Y, %H:%M:%S" -TimeZone -TimeZoneCity -DaylightSavings -FocalLength -ExposureTime -Model -ISO -s -csv -r /Volumes/INTENSO/Backups.backupdb/Matthias’s\ MacBook\ Pro/Latest/Macintosh\ HD/Users/matthiasweber/Pictures/$i > outputEXIFcsvs/PhotoEXIFDataDayType_$i.csv
+    exiftool -DateTimeOriginal -d "%b %d %a ,%Y, %H:%M:%S" -TimeZone -TimeZoneCity -DaylightSavings -FocalLength -ExposureTime -Model -ISO -Aperture -MeasuredEV -MeasuredRGGB -WB_RGGBLevelsAsShot -ColorTemperature -ColorTemperatureAsShot -ColorTempDaylight -LightValue -s -csv -r /Volumes/INTENSO/Backups.backupdb/Matthias’s\ MacBook\ Pro/Latest/Macintosh\ HD/Users/matthiasweber/Pictures/$i > outputEXIFcsvs/PhotoEXIFDataDayType_$i.csv
 done
