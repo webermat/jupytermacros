d750bd70925e4f9e1f15bc11ceda45f912b4b5fd
Matthias Artur Weber <matthias.artur.weber@cern.ch>
2020-07-09 16:44:30 +0200

    latest version of analysis files

--- extractEXIFData.sh
+++ extractEXIFData.sh
@@ -4,5 +4,5 @@ for i in {2006..2019}
 do
     echo "extract info from folder $i"
     exiftool -DateTimeOriginal -d "%b %d %a ,%Y, %H:%M:%S" -TimeZone -TimeZoneCity -DaylightSavings -FocalLength -ExposureTime -Model -ISO -Aperture -MeasuredEV -MeasuredRGGB -WB_RGGBLevelsAsShot -ColorTemperature -ColorTemperatureAsShot -ColorTempDaylight -LightValue -s -\
-csv -r ~/Pictures/$i > outputEXIFcsvs/PhotoEXIFDataDayType_$i.csv     
+csv -r ~/Pictures/$i > outputEXIFcsvsFullInfo/PhotoEXIFDataDayType_$i.csv     
 done
